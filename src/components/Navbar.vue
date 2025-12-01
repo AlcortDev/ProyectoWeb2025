91cd49b6e75e68e35c430b6e55fb988cf0d7837a
AlcortDev <alvarocortesdev@gmail.com>
2025-12-01 11:30:27 -0600

    nav agregado

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="navbar">
+    <button class="navbar_button">Inicio</button>
+    <button class="navbar_button">Nosotros</button>
+    <button class="navbar_button">Contacto</button>
+    <button class="navbar_button">Preguntas Frecuentes</button>
+</div>
+</template> 
+
+<style scoped>
+.navbar{
+    height: 50px;
+    width: 100%;
+    background-color: red;
+}
+</style>
+
+<script setup>
+</script>
